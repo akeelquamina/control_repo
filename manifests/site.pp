34326a5a8cbf6422c14fc31c8cd09978720ea467
akeelquamina <100498809+akeelquamina@users.noreply.github.com>
2022-06-30 13:42:30 -0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is a README hoss',
-    owner   => 'root',
-    }
-  file {'/root/README':
-    owner => 'root',
-  }
+
 }
