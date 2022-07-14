85c760c065fb60bf78f8d824f0a9e541679c1c2d
akeelquamina <100498809+akeelquamina@users.noreply.github.com>
2022-07-14 15:28:58 -0400

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC1nRMEIFp2bVMTA6p3K9mlWpb/SRIK+1C4EJqizErdXFG+/EUCa+ZYhAle+/2Cqb7MdPrB0tTDj0e6jKoSk716tIwtWCiQp2g4blGh8K0qOaBsiJ3BmCQU/3rYnkEeanvqiQKBMAuo6vh8KUvLFhP9ZABlLCiUDLE/LnSXrsURM24zHVUjggnVJ910Wim2ulkBEePcVuIiN5ji7tT/bgLFn4wr7264+vEdv0z2QeWCmlg7/jBYIZ07AqU4r37XZO/zy20O6b7h5IETrBd82eurx2Usggk4UfutW7vZL11WQf5unnJqQdvg/Zx9kO0f4TWVjYskz+1PQ9Tn+PvdQ3tH',
 	}  
 }
